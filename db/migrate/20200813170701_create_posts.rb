0e56562fe1fc1ee617b8d86eeb5c19479e3b32fa
keisuke-333 <keikubo.999@gmail.com>
2020-08-14 02:07:32 +0900

    Postモデルを作成

---
+++ db/migrate/20200813170701_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :language
+      t.text :code
+
+      t.timestamps
+    end
+  end
+end
