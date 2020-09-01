6443f2ab4c2bedfb7c57b967640e6083b15ad650
keisuke-333 <keikubo.999@gmail.com>
2020-09-01 16:46:50 +0900

    プロフィールのバリデーション文字数を変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-  validates :profile, length: { maximum: 100 }
+  validates :profile, length: { maximum: 200 }
   mount_uploader :image, ImageUploader
 
   has_many :posts
