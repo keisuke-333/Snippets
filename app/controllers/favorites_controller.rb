c62c4aaad45db8a74b59a1b10a0005736638191a
keisuke-333 <keikubo.999@gmail.com>
2020-09-10 22:33:46 +0900

    お気に入りボタンの機能を実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,13 @@
 class FavoritesController < ApplicationController
   def create
+    favorite = current_user.favorites.build(post_id: params[:post_id])
+    favorite.save
+    redirect_to post_path(favorite.post_id)
   end
 
   def destroy
+    favorite = Favorite.find_by(post_id: params[:post_id], user_id: current_user.id)
+    favorite.destroy
+    redirect_to post_path(favorite.post_id)
   end
 end
