0f9ce011b9ec94466f6a9c321ea772345c035ea7
keisuke-333 <keikubo.999@gmail.com>
2020-09-11 16:28:25 +0900

    ajaxでお気に入りボタンを実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,13 @@
 class FavoritesController < ApplicationController
   def create
+    @post = Post.find(params[:post_id])
     favorite = current_user.favorites.build(post_id: params[:post_id])
     favorite.save
-    redirect_to post_path(favorite.post_id)
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
     favorite = Favorite.find_by(post_id: params[:post_id], user_id: current_user.id)
     favorite.destroy
-    redirect_to post_path(favorite.post_id)
   end
 end
