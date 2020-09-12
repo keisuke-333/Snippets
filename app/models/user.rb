dae0fe8bde7cb03baef49306617ec48dd9ee1b63
keisuke-333 <keikubo.999@gmail.com>
2020-09-12 11:46:52 +0900

    userがfavしたpostをアソシエーションで取得

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
 
   has_many :posts, :dependent => :destroy
   has_many :favorites, :dependent => :destroy
+  has_many :favorite_posts, through: :favorites, source: :post
 
   def update_without_current_password(params, *options)
     params.delete(:current_password)
