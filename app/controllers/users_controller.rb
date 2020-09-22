4c8ec97009ee26bfc09147929c5885fb8027a3f1
keisuke-333 <keikubo.999@gmail.com>
2020-09-22 20:46:24 +0900

    投稿日の降順でリスト表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts
-    @favorite_posts = @user.favorite_posts
+    @posts = @user.posts.order(created_at: :desc)
+    @favorite_posts = @user.favorite_posts.order(created_at: :desc)
   end
 end
