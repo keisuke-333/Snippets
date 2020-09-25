e6892db635a4bbb87f24007a97a1c42953f6b857
keisuke-333 <keikubo.999@gmail.com>
2020-09-25 18:38:37 +0900

    ユーザーのフォロー済とフォロワーを取得

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = @user.posts.order(created_at: :desc)
     @favorite_posts = @user.favorite_posts.order(created_at: :desc)
+    @followings = @user.followings
+    @followers = @user.followers
   end
 end
