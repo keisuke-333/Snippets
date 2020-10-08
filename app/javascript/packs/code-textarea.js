0cfb061e2358a9983e9be4efdccb77f924159f21
keisuke-333 <keikubo.999@gmail.com>
2020-10-08 19:43:38 +0900

    SimpleMDEのツールバー表示項目を変更

--- app/javascript/packs/code-textarea.js
+++ app/javascript/packs/code-textarea.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const simplemde = new SimpleMDE({
     element: document.getElementById("post_code"),
     forceSync: true,
-    spellChecker: false
+    spellChecker: false,
+    toolbar: ['bold', 'heading', 'italic', 'code', 'unordered-list', 'ordered-list', '|', 'preview', 'side-by-side', 'fullscreen', '|', 'guide', ]
   });
 });
