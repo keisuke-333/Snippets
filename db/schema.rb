ac1b157889e30afddce63ea11bbe5cf3522e80e6
keisuke-333 <keikubo.999@gmail.com>
2020-11-01 22:39:00 +0900

    postsテーブルのuser_idカラムに外部キー制約を設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_01_124101) do
+ActiveRecord::Schema.define(version: 2020_11_01_133139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,4 +81,5 @@ ActiveRecord::Schema.define(version: 2020_11_01_124101) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
