233e94b0d05fbdbb94aea2e0c332278d0407f6ae
keisuke-333 <keikubo.999@gmail.com>
2020-11-03 20:42:29 +0900

    トップページに一部の投稿一覧を表示

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,8 @@
 class HomesController < ApplicationController
+  POSTS_NUM = 5
+
   def index
     redirect_to posts_url if user_signed_in?
+    @posts = Post.includes(:user, :favorites).order(impressions_count: "DESC").limit(POSTS_NUM)
   end
 end
