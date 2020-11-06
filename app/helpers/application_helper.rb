34d23db6332af5f603c82e36c14d8db5e63bddce
keisuke-333 <keikubo.999@gmail.com>
2020-11-06 22:18:11 +0900

    言語のアイコンを表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,16 @@ module ApplicationHelper
     processor = Qiita::Markdown::Processor.new(hostname: "example.com")
     processor.call(markdown)[:output].to_s.html_safe
   end
+
+  def language_icon(language)
+    if language == "HTML"
+      icon("fab", "html5", class: "fa-fw fa-lg html")
+    elsif language == "CSS"
+      icon("fab", "css3-alt", class: "fa-fw fa-lg css")
+    elsif language == "JavaScript"
+      icon("fab", "js", class: "fa-fw fa-lg js")
+    elsif language == "Ruby"
+      icon("fas", "gem", class: "fa-fw fa-lg ruby")
+    end
+  end
 end
