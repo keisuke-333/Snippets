f8ad200aedd7c2addebdfc75eca36497057bb40a
keisuke-333 <keikubo.999@gmail.com>
2020-11-27 13:29:02 +0900

    CloudFrontの設定

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,9 +4,9 @@ if Rails.env.production?
       provider: "AWS",
       aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
       aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-      region: Rails.application.credentials.dig(:aws, :region),
+      region: "ap-northeast-1",
     }
-    config.fog_directory = Rails.application.credentials.dig(:aws, :bucket)
-    config.asset_host = "https://d26soiw9xrcvtt.cloudfront.net"
+    config.fog_directory = "snippets-webdev-s3"
+    config.asset_host = "https://static.snippets-webdev.com"
   end
 end
